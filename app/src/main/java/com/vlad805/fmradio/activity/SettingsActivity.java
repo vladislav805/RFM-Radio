88b84e9d1fd571ce9ad28153bd10d49a5d101441
Vladislav Veluga <vladislav805@yandex.com>
2020-02-02 18:03:48 +0300

    Migrate to AppCompat SettingsActivity

--- app/src/main/java/com/vlad805/fmradio/activity/SettingsActivity.java
+++ app/src/main/java/com/vlad805/fmradio/activity/SettingsActivity.java
@@ -1,22 +1,37 @@
 package com.vlad805.fmradio.activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import com.vlad805.fmradio.R;
 import com.vlad805.fmradio.fragments.PreferencesFragment;
 
-@SuppressWarnings("deprecation")
 public class SettingsActivity extends AppCompatActivity {
 
-	private static final String TITLE_TAG = "title";
-
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.settings_activity);
+		setSupportActionBar(findViewById(R.id.settings_toolbar));
 		getSupportFragmentManager()
 				.beginTransaction()
 				.replace(R.id.settings, new PreferencesFragment())
 				.commit();
+
+		ActionBar ab = getSupportActionBar();
+
+		if (ab != null) {
+			ab.setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(final MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
