897ddf1487bc24c4e9165806083955b64070da5c
Vladislav Veluga <vladislav805@yandex.com>
2020-02-02 16:25:58 +0300

    Migrate to AppCompat AboutActivity

--- app/src/main/java/com/vlad805/fmradio/activity/AboutActivity.java
+++ app/src/main/java/com/vlad805/fmradio/activity/AboutActivity.java
@@ -1,20 +1,23 @@
 package com.vlad805.fmradio.activity;
 
-import android.app.ActionBar;
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import com.vlad805.fmradio.BuildConfig;
 import com.vlad805.fmradio.R;
 
-public class AboutActivity extends Activity {
+public class AboutActivity extends AppCompatActivity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
 
-		ActionBar ab = getActionBar();
+		setSupportActionBar(findViewById(R.id.about_toolbar));
+
+		ActionBar ab = getSupportActionBar();
 		if (ab != null) {
 			ab.setDisplayHomeAsUpEnabled(true);
 		}
