24028c189bd0eea8bbd8a82d18873054ec495b49
Vladislav Veluga <vladislav805@yandex.com>
2020-02-03 16:53:52 +0300

    Unified Poll & Request and implementations

---
+++ app/src/main/java/com/vlad805/fmradio/service/fm/communications/Request.java
@@ -0,0 +1,50 @@
+package com.vlad805.fmradio.service.fm.communications;
+
+/**
+ * vlad805 (c) 2020
+ */
+public class Request {
+	private final String command;
+	private final int timeout;
+	private OnReceivedResponse listener;
+
+	public interface OnReceivedResponse {
+		void onResponse(final String data);
+	}
+
+	public Request(final String command, final int timeout) {
+		this.command = command;
+		this.timeout = timeout;
+	}
+
+	public Request(final String command) {
+		this(command, 1000);
+	}
+
+	public Request onResponse(OnReceivedResponse listener) {
+		this.listener = listener;
+		return this;
+	}
+
+	public byte[] bytes() {
+		return command.getBytes();
+	}
+
+	public String getCommand() {
+		return command;
+	}
+
+	public int size() {
+		return command.length();
+	}
+
+	public int getTimeout() {
+		return timeout;
+	}
+
+	public void fire(final String result) {
+		if (listener != null) {
+			listener.onResponse(result);
+		}
+	}
+}
