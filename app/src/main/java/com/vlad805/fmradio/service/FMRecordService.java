4b546a2cb7f5a9462818cfe4ad3ca2c3abb51445
Vladislav Veluga <vladislav805@yandex.com>
2020-02-03 14:23:07 +0300

    Comments for RecordService

--- app/src/main/java/com/vlad805/fmradio/service/FMRecordService.java
+++ app/src/main/java/com/vlad805/fmradio/service/FMRecordService.java
@@ -24,26 +24,64 @@ public class FMRecordService implements IFMRecorder {
 	}
 
 	private final Context mContext;
+
+	/**
+	 * Current frequency
+	 */
 	private final int mKHz;
 
+	/**
+	 * State of recording
+	 */
 	private State mState = State.IDLE;
+
+	/**
+	 * Recordable file
+	 */
 	private File mRecordFile = null;
+
+	/**
+	 * Size of file in bytes
+	 */
 	private int mRecordLength = 0;
 
+	/**
+	 * Writable stream
+	 */
 	private BufferedOutputStream mBufferOutStream = null;
 	private FileOutputStream mFileOutStream = null;
 
 	private final int RECORD_BUFFER_SIZE = 1048576;
 
+	/**
+	 * Timestamp of start recording
+	 */
 	private long mStarted;
-	private long mLast;
-	private static final int DELTA = 750;
 
+	/**
+	 * Last broadcasting
+	 */
+	private long mLast;
+
+	/**
+	 * Minimal delay for
+	 */
+	private static final int MIN_DELAY_PING = 750;
+
+	/**
+	 * Constructor
+	 * @param context Context
+	 * @param kHz Current frequency in kHz
+	 */
 	public FMRecordService(final Context context, final int kHz) {
 		mContext = context;
 		mKHz = kHz;
 	}
 
+	/**
+	 * Returns state
+	 * @return State
+	 */
 	public State getState() {
 		return mState;
 	}
@@ -67,8 +105,8 @@ public class FMRecordService implements IFMRecorder {
 				mState = State.IDLE;
 			}
 
-			long now = System.currentTimeMillis();
-			if (now - mLast > DELTA) {
+			final long now = System.currentTimeMillis();
+			if (now - mLast > MIN_DELAY_PING) {
 				mLast = now;
 				updateState(C.Event.RECORD_TIME_UPDATE);
 			}
@@ -107,6 +145,10 @@ public class FMRecordService implements IFMRecorder {
 		updateState(C.Event.RECORD_ENDED);
 	}
 
+	/**
+	 * Send event. Also send size of file, duration and path
+	 * @param event Key of event
+	 */
 	private void updateState(final String event) {
 		mContext.sendBroadcast(new Intent(event)
 				.putExtra(C.Key.SIZE, mRecordLength)
@@ -115,12 +157,17 @@ public class FMRecordService implements IFMRecorder {
 		);
 	}
 
+	/**
+	 * Returns current duration of record
+	 * @return Duration in seconds
+	 */
 	private int getDuration() {
 		return (int) (System.currentTimeMillis() - mStarted) / 1000;
 	}
 
-	/***************************/
-
+	/**
+	 * Create file and initialize streams
+	 */
 	private void createFile() {
 		final File dir = makeDirectoryHierarchy();
 		final String name = getFilename();
@@ -180,7 +227,13 @@ public class FMRecordService implements IFMRecorder {
 		return String.format(Locale.ENGLISH, "FM-%s-%04d.wav", sdf.format(now), mKHz);
 	}
 
-
+	/**
+	 * Write bytes to buffer
+	 * @param buffer Buffer
+	 * @param index Offset
+	 * @param bytes Length
+	 * @param value Value
+	 */
 	private void writeBytes(byte[] buffer, int index, int bytes, int value) {
 		if (bytes > 0) {
 			buffer[index] = (byte) (value & 0xff);
@@ -196,6 +249,43 @@ public class FMRecordService implements IFMRecorder {
 		}
 	}
 
+	/**
+	 * Write WAV header data
+	 *  0… 3 (4 bytes) chunkId
+	 *                 ASCII symbols «RIFF» = 0x52494646. Beginning of RIFF-chunk.
+	 *  4… 7 (4 bytes) chunkSize
+	 *                 This is the remaining chain size starting at this position.
+	 *                 In other words, this is the fileSize-8, that is, the chunkId
+	 *                 and chunkSize fields are excluded.
+	 *  8…11 (4 bytes) format
+	 *                 Contains «WAVE» = 0x57415645
+	 * 12…15 (4 bytes) subchunk1Id
+	 * 	               Contains "fmt " = 0x666d7420
+	 * 16…19 (4 bytes) subchunk1Size
+	 * 	               16 for PCM format. This is the remaining size of the chain,
+	 * 	               starting from this position.
+	 * 20…21 (2 bytes) audioFormat
+	 *                Audio format, list of acceptable formats.
+	 * 	               For PCM = 1 (i.e., linear quantization).
+	 * 	               Values other than 1 indicate some compression format.
+	 * 22…23 (2 bytes) numChannels
+	 * 	               The number of channels. Mono = 1, Stereo = 2, etc.
+	 * 24…27 (4 bytes) sampleRate
+	 * 	               Sampling rate. 8000 Hz, 44100 Hz, etc.
+	 * 28…31 (4 bytes) byteRate
+	 * 	               The number of bytes transferred per second of playback.
+	 * 32…33 (2 bytes) blockAlign
+	 * 	               The number of bytes for one sample, including all channels.
+	 * 34…35 (2 bytes) bitsPerSample
+	 *                 The number of bits in the sample. The so-called "depth" or
+	 *                 sound accuracy. 8 bits, 16 bits, etc.
+	 * 36…39 (4 bytes) subchunk2Id
+	 * 	               Contains «data» = 0x64617461
+	 * 40…43 (4 bytes) subchunk2Size
+	 *                 The number of bytes in the data area.
+	 * 44…   (N bytes) data
+	 * 	               WAV data
+	 */
 	private void writeWavHeader() {
 		byte[] header = stringToByteArray("RIFF....WAVEfmt sc1safncsamrbytrbabsdatasc2s");
 
@@ -216,6 +306,11 @@ public class FMRecordService implements IFMRecorder {
 		}
 	}
 
+	/**
+	 * Returns string as byte array
+	 * @param s Input string
+	 * @return Byte array
+	 */
 	public static byte[] stringToByteArray(final String s) {
 		char[] buffer = s.toCharArray();
 		byte[] content = new byte[buffer.length];
@@ -225,15 +320,22 @@ public class FMRecordService implements IFMRecorder {
 		return content;
 	}
 
+	/**
+	 * Update header WAV
+	 */
 	private void writeFinal() {
 		try {
 			if (mRecordFile != null) {
-				RandomAccessFile file = new RandomAccessFile(mRecordFile, "rw");
+				final RandomAccessFile file = new RandomAccessFile(mRecordFile, "rw");
 
-				byte[] buffer = new byte[4];
+				final byte[] buffer = new byte[4];
+
+				// Set 4-7 bytes (length + 36)
 				writeBytes(buffer, 0, 4, mRecordLength + 36);
 				file.seek(4);
 				file.write(buffer);
+
+				// Set 40-43 bytes (length)
 				writeBytes(buffer, 0, 4, mRecordLength);
 				file.seek(40);
 				file.write(buffer);
